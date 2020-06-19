992cde1a03ca7dd800df2374a27f6bd9e6612abf
liqingyun <liqingyun.chn@qq.com>
2020-06-19 23:16:17 +0800

    111

--- utils/mysql.config.js
+++ utils/mysql.config.js
@@ -1,4 +1,4 @@
-const mysqlConfig = {
+const db = {
   host : 'localhost',
   port : 3306,
   user : 'root',
@@ -9,6 +9,6 @@ const mysqlConfig = {
 const secret = '42cf69058c27618f7d22902c86672e79'
 
 module.exports = {
-  mysqlConfig,
+  db,
   secret
 }
--- utils/mysql.js
+++ utils/mysql.js
@@ -1,28 +1,21 @@
 const mysql = require('mysql')
-const { mysqlConfig } = require('./mysql.config')
+const { db } = require('./mysql.config')
+const con = mysql.createConnection(db)
+con.connect()
+
+function exec(sql) {
+  const promise = new Promise((resolve, reject) => {
+    con.query(sql, (err, result) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(result)
+    })
+  })
+  return promise
+}
 
 module.exports = {
-  query: (sql, params, callback) => {
-    const connection = mysql.createConnection(mysqlConfig)
-    console.log(connection)
-    connection.connect((error) => {
-      if (error) {
-        console.log('数据库链接失败')
-        throw error
-      }
-      connection.query(sql, params, (error, results, fields) => {
-        if (error) {
-          console.log('数据操作失败')
-          throw error
-        }
-        callback && callback(results, fields)
-        connection.end((error) => {
-          if (error) {
-            console.log('关闭数据库连接失败！')
-            throw error
-          }
-        })
-      })
-    })
-  }
+  exec
 }
